17814190ccb3c2e8248b08d57652819aef3ac911
dolores3 <dolores@atom.nl>
2025-03-25 23:39:09 +0100

    layout /app file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Stack } from "expo-router/stack";
+import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+export default function Layout() {
+  const createDbIfNeeded = async (db: SQLiteDatabase) => {
+    console.log("creating database if needed")
+    await db.execAsync(
+      "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMAIRY KEY AUTOINCREMENT, firs_name TEXT last_name TEXT"
+    );
+  };
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <SQLiteProvider databaseName="test.db" onInit={createDbIfNeeded}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    </SQLiteProvider>
   );
 }
